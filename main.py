a98413aff2a5fb43029826bd9721219bafe92de4
Kocyk995436 <kocenkobartosz@gmail.com>
2021-02-25 11:12:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+#zad1
+#a = 5
+#b = 15
+#str1 = "napis 1"
+#str2 = "napis 2"
+#list1 = [1,2,3]
+#list2 = ["element1","element2"]
+
+#print(a)
+#print(b)
+#print(str1)
+#print(str2)
+#print(list1[2])
+#print(list2[0])
+
+#zad2
+# a= 5
+# b= 2
+# dodawanie= a+b
+# odejmowanie= a-b
+# dzielenie= b/a
+# dzielenie_calkowite= b/a
+# mnozenie= a*b
+# potegowanie= a**3
+# reszta= 4%3
+# print(sqrt(b))
+# print(dodawanie)
+# print(odejmowanie)
+# print(dzielenie)
+# print(dzielenie_calkowite)
+# print(mnozenie)
+# print(potegowanie)
+# print(reszta)
+
+#zad3
+#from math import
+#a = 5
+#a+=2
+#print(a)
+#a-=2
+#print(a)
+#a/=2
+#print(a)
+#a*=10
+#print(a)
+#a**=2
+#print(a)
+#a%=5a+=2
+#print(a)
+#a-=2
+#print(a)
+#a/=2
+#print(a)
+#a*=10
+#print(a)
+#a**=2
+#print(a)
+#a%=5
+#print(a)
+
+#zad4
+
+#e=5
+#e**=10
+#print(e)
+#int(pow(log(5+sin(8)**2),1/6))
+
+# zad5
+imie="BARTOSZ"
+nazwisko="KOCENKO"
+print(imie.capitalize() + nazwisko.capitalize())
+
+#zad6
+string = "la la la  kotki dwa"
+substring = "la"
+count = string.count(substring)
+print(count)
