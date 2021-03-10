2f6d87c81f82f8f67df8d0999cb7f77a7bf2607f
Kocyk995436 <73549265+Kocyk995436@users.noreply.github.com>
2021-03-10 14:43:20 +0100

    Add files via upload

---
+++ zajecia2.py
@@ -0,0 +1,69 @@
+# zad1
+# lista = ['nozna', 'siatkowka', 'koszykowka']
+# print(lista)
+# lista.reverse()
+# print(lista)
+# lista.append('kolarstwo')
+# print(lista)
+
+# zad2
+# slownik = {'dr': 'doktor', 'kat': 'kategoria', 'fot': 'fotografia', 'ang': 'angielski'}
+# print(slownik)
+
+# zad3
+#gry = {'akcji': 'battlefield', 'wyscigowe': 'asfalt', 'przygodowe': 'assasin'}
+#print(len(gry))
+
+#zad4
+#print("napisz zdanie")
+#x = input()
+#a = x.count("a")
+#b = x.count("A")
+#c = a + b
+#print("twoje zdanie" " " +x, "ma liter a:", + c)
+
+#zad6
+#a = 2
+#b = 4
+#c = 3
+#if a>b and a>c :
+#    print("a jest najwieksze")
+#elif b>a and b>c :
+ #   print("b jest najwieksze")
+#elif c>a and c>b :
+#    print("c jest najwieksze")
+#elif a == b and a > c and b > c :
+#    print("a i b sa najwieksze")
+#elif b == c and b > a and c > a :\
+#    print("b i c sa najwieksze")
+#elif a == c and a > b and c > b :
+#    print("a i c sa najwieksze")
+#    print(c)
+#else:
+#    print("wszystkie trzy sa rowne")
+
+#zad6
+#lista = [3, 7, 2, 2.5, 4.7, 5, 5.3]
+#for liczba in lista:
+#    print(liczba * liczba)
+
+#zad7
+# lista = []
+# i = 0
+#
+# for i in range (0, 10):
+#     if i % 2 == 0
+#         lista.append(i)
+# print(lista)
+
+#zad10
+import math
+print("podaj liczbe")
+a = input()
+if int(a)>0:
+    pierwiastek = math.sqrt(int(a))
+    print(pierwiastek)
+else:
+    print("blad")
+
+
